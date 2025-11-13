6b299e2ac3dc1f23e8b2fafe6d517af1b9d6fc5a
Prashant Rajput <prashantkumarrajput582008@gmail.com>
2025-11-13 20:32:08 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-chars = string.ascii_letters + string.digits
-length = int(input("Enter password length: "))
-password = ''.join(random.choice(chars) for _ in range(length))
-
-print("Generated Password:", password)
